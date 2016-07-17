8e5944f1f26527784c5d19bc2a193196e06681c9
sverweij <sverweij@users.noreply.github.com>
2016-07-17 10:32:02 +0200

    :wrench: improves the phantomjs <=> nodejs bridge error handling for bitmap graphics

--- src/cli-phantom.js
+++ src/cli-phantom.js
@@ -25,16 +25,21 @@ page.onError = function(pMessage /* , pTrace*/) {
     phantom.exit(1);
 };
 
-page.open(gPage, function(/* pStatus*/) {
-    page.injectJs(gRequirePath);
-    page.evaluate(
-        function(pASTString, pModuleBase, pStyleAdditions, pMirrorEntities, pAdditionalTemplate){
-            renderInThePage(pASTString, pModuleBase, pStyleAdditions, pMirrorEntities, pAdditionalTemplate);
-        },
-        gASTString,
-        gModuleBase,
-        gStyleAdditions,
-        gMirrorEntities,
-        gAdditionalTemplate
-    );
+page.open(gPage, function(pStatus) {
+    if (pStatus === "success") {
+        page.injectJs(gRequirePath);
+        page.evaluate(
+            function(pASTString, pModuleBase, pStyleAdditions, pMirrorEntities, pAdditionalTemplate){
+                renderInThePage(pASTString, pModuleBase, pStyleAdditions, pMirrorEntities, pAdditionalTemplate);
+            },
+            gASTString,
+            gModuleBase,
+            gStyleAdditions,
+            gMirrorEntities,
+            gAdditionalTemplate
+        );
+    } else {
+        console.error("failed to open ", gPage);
+        phantom.exit(1);
+    }
 });
