ae09b3ac4e2d6ac7fab030d20cd0c3b19fd99900
sverweij <sverweij@users.noreply.github.com>
2018-02-04 16:24:52 +0100

    test(showLicense): adds a test for showing the license

---
+++ test/showLicense.spec.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const chai   = require("chai");
+const expect = chai.expect;
+const showLicense = require("../dist/actions/showLicense");
+
+describe('showLicense()', () => {
+    it("returns the license", () => {
+        expect(
+            showLicense()
+        ).to.contain(
+            "GNU General Public License"
+        );
+    });
+
+    it("has the current year in it", () => {
+        expect(
+            showLicense()
+        ).to.contain(
+            `-${new Date().getUTCFullYear()}`
+        );
+    });
+});
